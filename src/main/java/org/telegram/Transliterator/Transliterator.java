28a9f913c40bd3bce07705c0e8a4c24a523ecf73
Danil <fusiaaan@gmail.com>
2023-04-19 18:25:49 +0400

    add translation method

---
+++ src/main/java/org/telegram/Transliterator/Transliterator.java
@@ -0,0 +1,44 @@
+package org.telegram.Transliterator;
+
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import ru.homyakin.iuliia.Schemas;
+import ru.homyakin.iuliia.Translator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Transliterator {
+    public List<Translator> schemas = new ArrayList<>(28);
+    
+    
+    private Transliterator() {}
+    
+    public SendMessage translateText(Update update) {
+        String translated = translit(update.getMessage().getText());
+        if (translated.equals(""))
+            translated = "Ошибка: в результате перевода сообщения получилась пустая строка.";
+    
+        Long chatId = update.getMessage().getChatId();
+        
+        SendMessage sm = SendMessage.builder()
+                                 .text(translated).chatId(chatId)
+                                 .build();
+        
+        return sm;
+    }
+    
+    private String translit(String str) {
+        return schemas.get(26).translate(str);
+    }
+    
+    public static Transliterator createTransliterator() {
+        Transliterator tr = new Transliterator();
+        for (Schemas schem : Schemas.values())
+            tr.schemas.add(new Translator(schem));
+        
+        return tr;
+    }
+    
+}
